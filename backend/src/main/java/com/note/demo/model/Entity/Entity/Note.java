1c0959ba7c06ffb35a04c38c474b054aee5af5ba
GinoJSB <ginohable14@gmail.com>
2025-01-11 16:14:31 -0500

    Add Note entity with basic attributes and JPA annotations

--- backend/src/main/java/com/note/demo/model/Entity/Entity/Note.java
+++ backend/src/main/java/com/note/demo/model/Entity/Entity/Note.java
@@ -1,4 +1,33 @@
-package com.note.demo.model.Entity.Entity;
+package com.note.demo.model.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private boolean archived = false;
+
+    @ElementCollection
+    private List<String> categories = new ArrayList<>(); // Campo para categorías
+
+    public Note() {}
+
+    public Note(Long id, String title, String content, boolean archived, List<String> categories) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.archived = archived;
+        this.categories = categories;
+    }
 }
