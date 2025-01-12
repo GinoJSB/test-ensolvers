4051dee182a5de4c04f99f226ede7e9903f89df6
GinoJSB <ginohable14@gmail.com>
2025-01-12 14:56:47 -0500

    Integrate updated service methods in NoteController

--- backend/src/main/java/com/note/demo/controller/NoteController.java
+++ backend/src/main/java/com/note/demo/controller/NoteController.java
@@ -1,4 +1,45 @@
 package com.note.demo.controller;
 
+import com.note.demo.model.DTO.NoteReqDTO;
+import com.note.demo.model.DTO.NoteResponseDTO;
+import com.note.demo.service.INoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
 public class NoteController {
+
+    @Autowired
+    private INoteService noteService;
+
+    @GetMapping
+    public List<NoteResponseDTO> getAllNotes() {
+        return noteService.getNotes();
+    }
+
+    @PostMapping
+    public ResponseEntity<NoteResponseDTO> createNote(@RequestBody NoteReqDTO noteReqDTO) {
+        NoteResponseDTO savedNote = noteService.saveNote(noteReqDTO);
+        return ResponseEntity.ok(savedNote);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NoteResponseDTO> getNoteById(@PathVariable Long id) {
+        return ResponseEntity.ok(noteService.findNoteById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<NoteResponseDTO> updateNote(@PathVariable Long id, @RequestBody NoteReqDTO noteReqDTO) {
+        return ResponseEntity.ok(noteService.updateNote(id, noteReqDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNoteById(@PathVariable Long id) {
+        noteService.deleteNote(id);
+        return ResponseEntity.noContent().build();
+    }
 }
